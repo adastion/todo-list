b5967d407a9ee0a2301ec2d2293d69395241025b
Andrey Sery <andrey.72006@gmail.com>
2024-02-03 00:37:22 +0300

    save

--- src/features/counter/Counter.jsx
+++ src/features/counter/Counter.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { decrement, increment } from "./counterSlice";
+import S from "./Counter.module.scss";
 
 export function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <div className={S.container}>
       <div>
         <button
           aria-label="Increment value"
